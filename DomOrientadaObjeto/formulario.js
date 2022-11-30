fae5768e1187f063f6af171041426b314987db28
Fábio Rocha <Fabiorocharb@gmail.com>
2022-11-29 22:36:17 -0300

    evento

--- DomOrientadaObjeto/formulario.js
+++ DomOrientadaObjeto/formulario.js
@@ -1,13 +1,14 @@
-function pegarDados(evento) {
-  evento.preventDefault();
+function pegarDados(event) {
+  event.preventDefault();
 
   const formulario = document.querySelector("form");
   const dados = {};
 
   [...formulario.querySelectorAll("input")].forEach((tag) => {
     dados[tag.name] = tag.value;
+    sessionStorage.setItem(tag,name, dados[tag.name]);
   });
-  console.log(dados);
+  console.log(sessionStorage);
 
   const form = new FormData(formulario);
   console.log(form.get("nome"));
